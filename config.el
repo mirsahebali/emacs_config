1e53ae42daecfa8983616c6333ecf28c96e453a9
Mir Saheb Ali <mirsahebali@protonmail.com>
2023-12-13 21:03:17 +0530

    Added poly mode persp mode

--- config.el
+++ config.el
@@ -212,20 +212,18 @@
             "C-k" '(evil-window-up :wk "Window up")
             "C-l" '(evil-window-right :wk "Window right")
 )
+(general-define-key
+        :states '(normal visual emacs)
+            "M-j" '(evil-collection-unimpaired-move-text-down :wk "Move Text Down")
+            "M-k" '(evil-collection-unimpaired-move-text-up :wk "Move Text Up"))
 )
 
-(use-package undo-tree
-:config
-
-(setq undo-tree-auto-save-history t)
-(setq undo-tree-history-directory-alist '(("." . "~/.config/emacs/undo")))
-(setq undo-limit (* 100 1024 1024)) ;; 100MB.
-(setq undo-strong-limit undo-limit)
-(setq undo-tree-visualizer-timestamps t)
-(setq undo-tree-visualizer-diff t)
-
-(global-undo-tree-mode 1)
-)
+(use-package undo-fu
+  :config
+  (global-unset-key (kbd "C-z"))
+  (global-set-key (kbd "C-z")   'undo-fu-only-undo)
+  (global-set-key (kbd "C-S-z") 'undo-fu-only-redo))
+(use-package undo-fu-session)
 
 (electric-pair-mode)
 
@@ -333,6 +331,14 @@
    :config
 (ac-config-default)
    )
+
+;;(use-package polymode
+;;:ensure t))
+
+;;(use-package poly-markdown
+ ;; :ensure t
+;;:config
+;;(add-to-list 'auto-mode-alist '("\\.md" . poly-markdown-mode)))
 
 (use-package flycheck
   :ensure t
@@ -538,6 +544,7 @@ Other buffer group by `centaur-tabs-get-group-name' with project name."
     (load-theme 'catppuccin))
 
 (use-package hl-todo
+:ensure t
 :config
 (setq hl-todo-keyword-faces
       '(("TODO"   . "#FF0000")
@@ -549,7 +556,7 @@ Other buffer group by `centaur-tabs-get-group-name' with project name."
         ("REVIEW". "#FFC0CB")
         ("PERF" . "#7D7EEC")
         ("STUB"   . "#1E90FF")))
-        (hl-line-mode)
+
         (hl-todo-mode)
 )
 
@@ -576,7 +583,7 @@ Other buffer group by `centaur-tabs-get-group-name' with project name."
   (setq dashboard-banner-logo-title "R.T.F.M.  Run The Funking Monad")
   ;;(setq dashboard-startup-banner 'logo) ;; use standard emacs logo as banner
   (setq dashboard-startup-banner "/home/mirsahebali/.config/emacs/images/Arch-linux-logo.png")  ;; use custom image as banner
-  (setq dashboard-center-content nil) ;; set to 't' for centered content
+  (setq dashboard-center-content t) ;; set to 't' for centered content
   (setq dashboard-items '((recents . 5)
                           (agenda . 5 )
                           (bookmarks . 3)
@@ -587,120 +594,40 @@ Other buffer group by `centaur-tabs-get-group-name' with project name."
   :config
   (dashboard-setup-startup-hook))
 
-(use-package treemacs
-  :ensure t
-  :defer t
-  :init
-  (with-eval-after-load 'winum
-    (define-key winum-keymap (kbd "M-0") #'treemacs-select-window))
-  :config
-  (progn
-    (setq treemacs-collapse-dirs                   (if treemacs-python-executable 3 0)
-          treemacs-deferred-git-apply-delay        0.5
-          treemacs-directory-name-transformer      #'identity
-          treemacs-display-in-side-window          t
-          treemacs-eldoc-display                   'simple
-          treemacs-file-event-delay                2000
-          treemacs-file-extension-regex            treemacs-last-period-regex-value
-          treemacs-file-follow-delay               0.2
-          treemacs-file-name-transformer           #'identity
-          treemacs-follow-after-init               t
-          treemacs-expand-after-init               t
-          treemacs-find-workspace-method           'find-for-file-or-pick-first
-          treemacs-git-command-pipe                ""
-          treemacs-goto-tag-strategy               'refetch-index
-          treemacs-header-scroll-indicators        '(nil . "^^^^^^")
-          treemacs-hide-dot-git-directory          t
-          treemacs-indentation                     2
-          treemacs-indentation-string              " "
-          treemacs-is-never-other-window           nil
-          treemacs-max-git-entries                 5000
-          treemacs-missing-project-action          'ask
-          treemacs-move-forward-on-expand          nil
-          treemacs-no-png-images                   nil
-          treemacs-no-delete-other-windows         t
-          treemacs-project-follow-cleanup          nil
-          treemacs-persist-file                    (expand-file-name ".cache/treemacs-persist" user-emacs-directory)
-          treemacs-position                        'left
-          treemacs-read-string-input               'from-child-frame
-          treemacs-recenter-distance               0.1
-          treemacs-recenter-after-file-follow      nil
-          treemacs-recenter-after-tag-follow       nil
-          treemacs-recenter-after-project-jump     'always
-          treemacs-recenter-after-project-expand   'on-distance
-          treemacs-litter-directories              '("/node_modules" "/.venv" "/.cask")
-          treemacs-project-follow-into-home        nil
-          treemacs-show-cursor                     nil
-          treemacs-show-hidden-files               t
-          treemacs-silent-filewatch                nil
-          treemacs-silent-refresh                  nil
-          treemacs-sorting                         'alphabetic-asc
-          treemacs-select-when-already-in-treemacs 'move-back
-          treemacs-space-between-root-nodes        t
-          treemacs-tag-follow-cleanup              t
-          treemacs-tag-follow-delay                1.5
-          treemacs-text-scale                      nil
-          treemacs-user-mode-line-format           nil
-          treemacs-user-header-line-format         nil
-          treemacs-wide-toggle-width               70
-          treemacs-width                           35
-          treemacs-width-increment                 1
-          treemacs-width-is-initially-locked       t
-          treemacs-workspace-switch-cleanup        nil)
+(use-package neotree
+:init
+(setq neo-theme (if (display-graphic-p) 'icons 'arrow))
+)
 
-    ;; The default width and height of the icons is 22 pixels. If you are
-    ;; using a Hi-DPI display, uncomment this to double the icon size.
-    ;;(treemacs-resize-icons 44)
-
-    (treemacs-follow-mode t)
-    (treemacs-filewatch-mode t)
-    (treemacs-fringe-indicator-mode 'always)
-    (when treemacs-python-executable
-      (treemacs-git-commit-diff-mode t))
-
-    (pcase (cons (not (null (executable-find "git")))
-                 (not (null treemacs-python-executable)))
-      (`(t . t)
-       (treemacs-git-mode 'deferred))
-      (`(t . _)
-       (treemacs-git-mode 'simple)))
-
-    (treemacs-hide-gitignored-files-mode nil))
-  :bind
-  (:map global-map
-        ("M-0"       . treemacs-select-window)
-        ("C-x t 1"   . treemacs-delete-other-windows)
-        ("C-x t t"   . treemacs)
-        ("C-x t d"   . treemacs-select-directory)
-        ("C-x t B"   . treemacs-bookmark)
-        ("C-x t C-t" . treemacs-find-file)
-        ("C-x t M-t" . treemacs-find-tag)))
-
+(use-package treemacs)
 (use-package treemacs-evil
-  :after (treemacs evil)
-  :ensure t)
+ :after (treemacs evil)
+:ensure t
+:config
+(treemacs-load-theme "Idea")
+)
 
 (use-package treemacs-projectile
-  :after (treemacs projectile)
+ :after (treemacs evil projectile)
   :ensure t)
 
 (use-package treemacs-icons-dired
-  :hook (dired-mode . treemacs-icons-dired-enable-once)
+ :hook (dired-mode . treemacs-icons-dired-enable-once)
   :ensure t)
 
 (use-package treemacs-magit
-  :after (treemacs magit)
+ :after (treemacs evil magit)
   :ensure t)
 
 (use-package treemacs-persp ;;treemacs-perspective if you use perspective.el vs. persp-mode
-  :after (treemacs persp-mode) ;;or perspective vs. persp-mode
+ :after (treemacs persp-mode) ;;or perspective vs. persp-mode
   :ensure t
-  :config (treemacs-set-scope-type 'Perspectives))
+ :config (treemacs-set-scope-type 'Perspectives))
 
 (use-package treemacs-tab-bar ;;treemacs-tab-bar if you use tab-bar-mode
-  :after (treemacs)
+ :after (treemacs)
   :ensure t
-  :config (treemacs-set-scope-type 'Tabs))
+ :config (treemacs-set-scope-type 'Tabs))
 
 (use-package doom-modeline
 :ensure t
@@ -795,3 +722,13 @@ one, an error is signaled."
   (interactive)
   (load-file user-init-file)
   (load-file user-init-file))
+
+(add-to-list 'display-buffer-alist
+             '("\\`\\*shell\\*\\(?:<[[:digit:]]+>\\)?\\'"
+               (display-buffer-in-side-window (side . bottom))))
+
+(use-package literate-calc-mode
+  :ensure t)
+
+(use-package transient
+:ensure t)
--- init.el
+++ init.el
@@ -3,9 +3,9 @@
   "config.org"
   user-emacs-directory))
 
-(set-frame-parameter nil 'alpha-background 90)
+(set-frame-parameter nil 'alpha-background 60)
 
-(add-to-list 'default-frame-alist '(alpha-background . 90))
+(add-to-list 'default-frame-alist '(alpha-background . 70))
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
@@ -13,7 +13,22 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   '("4990532659bb6a285fee01ede3dfa1b1bdf302c5c3c8de9fad9b6bc63a9252f7" "8c7e832be864674c220f9a9361c851917a93f921fedb7717b1b5ece47690c098" "014cb63097fc7dbda3edf53eb09802237961cbb4c9e9abd705f23b86511b0a69" "4594d6b9753691142f02e67b8eb0fda7d12f6cc9f1299a49b819312d6addad1d" "aec7b55f2a13307a55517fdf08438863d694550565dee23181d2ebd973ebd6b8" "0527c20293f587f79fc1544a2472c8171abcc0fa767074a0d3ebac74793ab117" default)))
+   '("d23073a9616156a16aecbd3d38e1c3a1f006fc5d920e3fbcb681411e35d2a096" "4990532659bb6a285fee01ede3dfa1b1bdf302c5c3c8de9fad9b6bc63a9252f7" "8c7e832be864674c220f9a9361c851917a93f921fedb7717b1b5ece47690c098" "014cb63097fc7dbda3edf53eb09802237961cbb4c9e9abd705f23b86511b0a69" "4594d6b9753691142f02e67b8eb0fda7d12f6cc9f1299a49b819312d6addad1d" "aec7b55f2a13307a55517fdf08438863d694550565dee23181d2ebd973ebd6b8" "0527c20293f587f79fc1544a2472c8171abcc0fa767074a0d3ebac74793ab117" default))
+ '(go-impl-aliases-alist '(("hh" . "http.Handler") ("irw" . "io.ReadWriter")))
+ '(newsticker-url-list
+   '(("" "https://planet.kde.org/global/atom.xml/" nil nil nil)
+     ("" "https://vkc.sh/feed/" nil nil nil)
+     ("" "https://www.developer.com/open-source/feed/" nil nil nil)
+     ("" "https://dev.to/rss" nil nil nil)
+     ("" "https://martinfowler.com/feed.atom" nil nil nil)
+     ("" "https://medium.com/feed/airbnb-engineering" nil nil nil)
+     ("" "http://feeds.feedburner.com/se-radio" nil nil nil)
+     ("" "https://www.toptal.com/blog.rss" nil nil nil)
+     ("" "https://netflixtechblog.com/feed" nil nil nil)
+     ("" "https://www.uber.com/en-IN/blog/mumbai/engineering/rss/" nil nil nil)
+     ("" "https://blog.codinghorror.com/rss/" nil nil nil)
+     ("" "https://catonmat.net/feed" nil nil nil)
+     ("" "https://medium.engineering/feed" nil nil nil))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
